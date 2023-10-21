8e9efd0fb1de1c353ad92ecb56440f03caa46a65
Khan <batyrkhan.akzholov@gmail.com>
2023-10-21 13:32:56 +0200

    Add CORS

--- EReceipt.Core/EReceipt.Core/Program.cs
+++ EReceipt.Core/EReceipt.Core/Program.cs
@@ -18,9 +18,14 @@ namespace EReceipt.Core
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
 
             var app = builder.Build();
 
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
